aca63e655ca0d364a35851de3c70d6e25cad6349
Juan Lopes <me@juanlopes.net>
2017-02-21 12:47:46 -0300

    code

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.warn("Hello world!");
+    }
+}
